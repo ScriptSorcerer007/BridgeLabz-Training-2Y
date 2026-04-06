6b09f274a164643dc156f942116410cb81e1f222
ScriptSorcerer007 <prakharj754@gmail.com>
2026-04-06 15:49:33 +0530

    Prakhar add tree solution

---
+++ Trees/PostOrder.java
@@ -0,0 +1,40 @@
+public class PostOrder {
+
+    // Node class
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    // Postorder Traversal: Left -> Right -> Root
+    public static void postorder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.data + " ");
+    }
+
+    public static void main(String[] args) {
+
+        // Creating Binary Tree
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        System.out.print("Postorder Traversal: ");
+        postorder(root);
+    }
+}
